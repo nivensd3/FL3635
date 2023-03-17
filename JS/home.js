5130b4d44e9d7d0551912a7b8774bb86d6dbcd3b
nivensd3 <nivensd3@gmail.com>
2023-03-17 10:13:33 -0400

    all

--- JS/home.js
+++ JS/home.js
@@ -22,76 +22,14 @@ var btn = document.getElementById("myBtn");
     }
   }
 
-  //Slideshow
-    
- function showSlides(slideIndex) {
-      let i;
-       let slides = document.getElementsByClassName("mySlides");
-       let dots = document.getElementsByClassName("dot");
-       for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {
-           slideIndex = 1;
-        }  
-
-       for (i = 0; i < dots.length; i++) {
-         dots[i].className = dots[i].className.replace(" active", "");
-       }
-       console.log(slideIndex);
-       console.log(slides[slideIndex-1]);
-       slides[slideIndex-1].style.display = "block";  
-       dots[slideIndex-1].classList.add("active");
-     }
-
-     let slideIndex = 0;
-     showSlides(slideIndex);
-
+  $(document).ready(function() {
+    var myVideo = document.getElementById("my-video");
   
-  
-     function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-     setTimeout(showSlides, 2000);
-
-//let slideIndex = 0;
-//showSlides();
-
-//function showSlides() {
-//  let i;
-//  let slides = document.getElementsByClassName("mySlides");
-//  let dots = document.getElementsByClassName("dot");
-//  for (i = 0; i < slides.length; i++) {
-//    slides[i].style.display = "none";  
- // }
- // slideIndex++;
- // if (slideIndex > slides.length) {slideIndex = 1}    
- // for (i = 0; i < dots.length; i++) {
- //   dots[i].className = dots[i].className.replace(" active", "");
- // }
- // slides[slideIndex-1].style.display = "block";  
- // dots[slideIndex-1].className += " active";
- // setTimeout(showSlides, 2000); // Change image every 2 seconds
-//}
-
-//Scroll to Top
-
-// Get the button
-let mybutton = document.getElementById("myBtnu");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+    myVideo.addEventListener("play", function() {
+      $(this).css({
+        "transform": "scale(1.2)",
+        "transform-origin": "center",
+        "margin": "0 auto"
+      });
+    }, false);
+  });
